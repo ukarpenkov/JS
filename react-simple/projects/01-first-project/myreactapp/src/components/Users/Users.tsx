8264c3395a8b66ed625a337be2c6ed6ab317fbd8
Ukarpenkov <truloveu@gmail.com>
2021-08-20 16:34:55 +0300

    search users Formik

--- react-simple/projects/01-first-project/myreactapp/src/components/Users/Users.tsx
+++ react-simple/projects/01-first-project/myreactapp/src/components/Users/Users.tsx
@@ -3,6 +3,7 @@ import s from './Users.module.css'
 import Paginator from '../common/Paginator/Paginator'
 import UserItem from './UserItem'
 import { UserType } from '../../types/types'
+import { Field, Form, Formik } from 'formik'
 
 type PropsType = {
   totalUsersCount: number
@@ -25,6 +26,7 @@ let Users: React.FC<PropsType> = ({
 }) => {
   return (
     <div>
+      <div></div>
       <Paginator
         currentPage={currentPage}
         onPageChanged={onPageChanged}
@@ -46,86 +48,43 @@ let Users: React.FC<PropsType> = ({
   )
 }
 
+const usersSearchFormValidate = (values: any) => {
+  const errors = {}
+
+  return errors
+}
+
+type UsersSearchFormObjectType = {
+  term: string
+}
+
+const UsersSearchForm = () => {
+  const submit = (
+    values: UsersSearchFormObjectType,
+    { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
+  ) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2))
+      setSubmitting(false)
+    }, 400)
+  }
+  return (
+    <div>
+      <Formik
+        initialValues={{ term: '' }}
+        validate={usersSearchFormValidate}
+        onSubmit={submit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="term" />
+            <button type="submit" disabled={isSubmitting}>
+              Найти
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
 export default Users
-
-// let Users = (props) => {
-//     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
-//     let pages = []
-//     for (let i = 1; i <= pagesCount; i++) {
-//         pages.push(i)
-//     }
-
-//     return < div >
-//         <div>
-//             {pages.map(p => {
-//                 return <span className={props.currentPage === p && s.selectedPage}
-//                     onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
-//             })}
-//         </div>
-//         {
-//             props.users.map(u => <div key={u.id}>
-//                 <span>
-//                     <div>
-//                         <NavLink to={'/profile/' + u.id}>
-//                             <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} />
-//                         </NavLink>
-//                     </div>
-//                     <div>
-//                         {u.followed
-//                             ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-//                                 props.toggleFollowingProgress(true, u.id)
-//                                 axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
-//                                     withCredentials: true,
-//                                     headers: {
-//                                         "API-KEY": "d40d50fa-1ffd-46f2-b0cd-1e36d2b2c38f"
-//                                     }
-//                                 })
-//                                     .then(response => {
-//                                         if (response.data.resultCode == 0) {
-//                                             props.unfollow(u.id)
-//                                         }
-//                                         props.toggleFollowingProgress(false, u.id)
-//                                     })
-//                             }}>Unfollow</button>
-//                             : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-//                                 props.toggleFollowingProgress(true, u.id)
-//                                 axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
-//                                     withCredentials: true,
-//                                     headers: {
-//                                         "API-KEY": "d40d50fa-1ffd-46f2-b0cd-1e36d2b2c38f"
-//                                     }
-//                                 })
-//                                     .then(response => {
-//                                         if (response.data.resultCode == 0) {
-//                                             props.follow(u.id)
-//                                         }
-//                                         props.toggleFollowingProgress(false, u.id)
-//                                     })
-//                             }}>Follow</button>}
-
-//                     </div>
-//                 </span>
-//                 <span>
-//                     <span>
-//                         <div>{u.name}</div>
-//                         <div>{u.status}</div>
-//                     </span>
-//                     <span>
-//                         <div>{'u.location.country'}</div>
-//                         <div>{'u.location.city'}</div>
-//                     </span>
-//                 </span>
-//             </div>)
-//         }
-//     </ div >
-// }
-
-// <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-//                                 props.toggleFollowingProgress(true, u.id)
-//                                 usersAPI.follow(u.id).then(data => {
-//                                     if (data.resultCode == 0) {
-//                                         props.follow(u.id)
-//                                     }
-//                                     props.toggleFollowingProgress(false, u.id)
-//                                 })
-//                             }}>Follow</button>}
