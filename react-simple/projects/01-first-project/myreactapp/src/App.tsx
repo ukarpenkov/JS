27e7affbe89d78a69f039a3d87bee2b89f75e6eb
Ukarpenkov <truloveu@gmail.com>
2021-09-09 17:14:57 +0300

    add chat with webSocket

--- react-simple/projects/01-first-project/myreactapp/src/App.tsx
+++ react-simple/projects/01-first-project/myreactapp/src/App.tsx
@@ -1,15 +1,16 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
 import {
   BrowserRouter,
+  Link,
+  NavLink,
   Redirect,
   Route,
   Switch,
   withRouter,
 } from 'react-router-dom'
-
-import HeaderContainer from './components/Header/HeaderContainer'
+import 'antd/dist/antd.css'
+import Header from './components/Header/Header'
 import { LoginPage } from './components/Login/LoginPage'
 import { connect, Provider } from 'react-redux'
 import { compose } from 'redux'
@@ -18,10 +19,18 @@ import Preloader from './components/common/preloader/Preloader'
 import store, { AppStateType } from './redux/redux-store'
 import { withSuspense } from './hoc/withSuspense'
 import { UsersPage } from './components/Users/UsersContainer'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons'
+
 //import DialogsContainer from './components/Dialogs/DialogsContainer.jsx';
 const DialogsContainer = React.lazy(
   () => import('./components/Dialogs/DialogsContainer')
 )
+const ChatPage = React.lazy(() => import('./pages/Chat/ChatPage'))
 // import ProfileContainer from './components/Profile/ProfileContainer'
 const ProfileContainer = React.lazy(
   () => import('./components/Profile/ProfileContainer')
@@ -34,6 +43,7 @@ type DispatchPropsType = {
 
 const SuspendedDialogs = withSuspense(DialogsContainer)
 const SuspendedProfile = withSuspense(ProfileContainer)
+const SuspendedChatPage = withSuspense(ChatPage)
 
 class App extends React.Component<MapPropsType & DispatchPropsType> {
   catchAllUnhandledErrors = (e: PromiseRejectionEvent) => {
@@ -47,32 +57,100 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
   // componentWillUnmount() {
   //     window.removeEventListener("unhandledrejection", this.catchAllUnhandledErrors);
   // }
+
   render() {
     if (!this.props.initialized) {
       return <Preloader />
     }
+    const { SubMenu } = Menu
+    const { Content, Footer, Sider } = Layout
     return (
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
+      <Layout>
+        <Header />
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Layout
+            className="site-layout-background"
+            style={{ padding: '24px 0' }}
+          >
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                // defaultSelectedKeys={['6']}
+                style={{ height: '100%' }}
+              >
+                <SubMenu key="sub1" icon={<UserOutlined />} title="Мой профиль">
+                  <Menu.Item key="1">
+                    {' '}
+                    <Link to="/profile">Профиль</Link>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    {' '}
+                    <Link to="/dialogs">Сообщения</Link>
+                  </Menu.Item>
 
-        <div className="app-wrapper-content">
-          <Switch>
-            <Route exact path="/" render={() => <Redirect to="/profile" />} />
-            <Route path="/dialogs" render={() => <SuspendedDialogs />} />
-            <Route
-              path="/profile/:userId?"
-              render={() => <SuspendedProfile />}
-            />
-            <Route
-              path="/users"
-              render={() => <UsersPage pageTitle={'Название страницы'} />}
-            />
-            <Route path="/login" render={() => <LoginPage />} />
-            <Route path="*" render={() => <div>404 not found</div>} />
-          </Switch>
-        </div>
-      </div>
+                  <Menu.Item key="3">
+                    {' '}
+                    <Link to="/news">News</Link>
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    <Link to="music">Music</Link>
+                  </Menu.Item>
+                  <Menu.Item key="5">
+                    {' '}
+                    <Link to="settings">Settings</Link>
+                  </Menu.Item>
+                </SubMenu>
+                <SubMenu
+                  key="sub2"
+                  icon={<LaptopOutlined />}
+                  title="Пользователи"
+                >
+                  <Menu.Item key="6">
+                    {' '}
+                    <Link to="/users">Пользователи</Link>
+                  </Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<NotificationOutlined />} title="Чат">
+                  <Menu.Item key="7">
+                    {' '}
+                    <Link to="/chat">Чат</Link>
+                  </Menu.Item>
+                  <Menu.Item key="8">option10</Menu.Item>
+                  <Menu.Item key="9">option11</Menu.Item>
+                  <Menu.Item key="10">option12</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={() => <Redirect to="/profile" />}
+                />
+                <Route path="/dialogs" render={() => <SuspendedDialogs />} />
+                <Route
+                  path="/profile/:userId?"
+                  render={() => <SuspendedProfile />}
+                />
+                <Route
+                  path="/users"
+                  render={() => <UsersPage pageTitle={'Название страницы'} />}
+                />
+                <Route path="/login" render={() => <LoginPage />} />
+                <Route path="/chat" render={() => <SuspendedChatPage />} />
+                <Route path="*" render={() => <div>404 not found</div>} />
+              </Switch>
+            </Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Социальная сеть 2021</Footer>
+      </Layout>
     )
   }
 }
--- react-simple/projects/01-first-project/myreactapp/src/components/Header/Header.tsx
+++ react-simple/projects/01-first-project/myreactapp/src/components/Header/Header.tsx
@@ -1,31 +1,54 @@
 import React from 'react'
-import s from './Header.module.css'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { Layout, Menu, Avatar, Row, Col, Button } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  selectCurrentUserLogin,
+  selectIsAuth,
+} from '../../redux/auth-selectors'
+import { logout } from '../../redux/auth-reducer'
+export type MapPropsType = {}
 
-export type MapPropsType = {
-  isAuth: boolean
-  login: string | null
-}
-
-export type DispatchPropsType = {
-  logout: () => void
-}
-
-const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
+const Header: React.FC<MapPropsType> = (props) => {
+  const isAuth = useSelector(selectIsAuth)
+  const login = useSelector(selectCurrentUserLogin)
+  const dispatch = useDispatch()
+  const logoutCallback = () => {
+    dispatch(logout())
+  }
+  const { Header } = Layout
   return (
-    <header className={s.header}>
-      <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png" />
+    <Header className="header">
+      <div className="logo" />
+      <Row>
+        <Col span={16}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">
+              {' '}
+              <NavLink to="/users">Пользователи</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Col>
 
-      <div className={s.loginBlock}>
-        {props.isAuth ? (
-          <div>
-            {props.login} - <button onClick={props.logout}>Log out</button>{' '}
-          </div>
+        <Avatar
+          style={{ backgroundColor: '#87d068' }}
+          icon={<UserOutlined />}
+        />
+        {isAuth ? (
+          <Col span={3}>
+            {login} - <Button onClick={logoutCallback}>Log out</Button>{' '}
+          </Col>
         ) : (
-          <NavLink to={'/login'}>Login</NavLink>
+          <Col span={3}>
+            <Button>
+              {' '}
+              <Link to={'/login'}>Login</Link>{' '}
+            </Button>
+          </Col>
         )}
-      </div>
-    </header>
+      </Row>
+    </Header>
   )
 }
 
